624c9c97e29463c747bc70c6dc302ae2ecac5892
Jussi <jussir@gmail.com>
2011-11-18 22:27:01 +0200

    Updated gitignore, added logging support, refactored some bits

--- pycrack.py
+++ pycrack.py
@@ -2,9 +2,10 @@
 import hashlib
 import urllib2
 import re
+import logging
 from sys import argv
 
-# Very simple md5 cracking with Google, idea taken from 
+# Very simple md5 cracking with Google, idea taken from
 # https://github.com/juuso/BozoCrack/blob/master/BozoCrack.rb
 
 # Feel free to use =)
@@ -12,42 +13,86 @@ from sys import argv
 class PyCrack:
     """ PyCrack tries to google each hash trying to find
 	an occurrence """
-    def __init__(self, filename):
+    def __init__(self, filename, wordlist = None):
+        logging.basicConfig(
+          level=logging.DEBUG,
+          format='(%(threadName)-10s) %(message)s')
+        self.logger = logging.getLogger()
         self.hashes = []
         fhandle = open(filename,'r')
         hash_list = fhandle.read()
-        self.hashes = re.findall(r"([a-fA-F\d]{32})", str(hash_list))
-        self.hashes = list(set(self.hashes))  # removes duplicates
-
+        hashes = re.findall(r"([a-fA-F\d]{32})", str(hash_list))
+        self.hashes = list(set(hashes))  # removes duplicates
+        
+        # Initialize wordlist
+        self.wordlist = self.init_wordlist(wordlist)
+        
+        # Urllib2 client
+        self.opener = urllib2.build_opener()
+        self.opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        
+        # File handlers
+        self.found_file = open('results/found.txt', 'a+')
+        self.not_found_file = open('results/notfound.txt', 'a+')
+        self.result_file  = open('results/results.txt', 'a+')
+    
+    def init_wordlist(self, wl):
+        return []
+    
+    def found(self, result):
+        """ Writes found string to file """
+        if result:
+            self.found_file.write(result + '\n')
+    
+    def not_found(self, result):
+        """ Writes to "not found" file """
+        if result:
+            self.not_found_file.write(result + '\n')
+    
+    def save_result(self, result):
+        """Saves found hash to output.txt"""
+        if result:
+            self.result_file.write(result + '\n')
+    
     def dictionary_attack(self, hash, wordlist):
-        flag = False
+        """ Goes throught the wordlist hash trying to find the hash. """
+        occurrences = False
         for key in wordlist:
             if hashlib.md5(key).hexdigest() == hash:
                 result = "%s:%s" % (hash,key,)
-                print result
-                flag = True
+                print "Found %s " % result
+                occurrences = True
+                # Saves hash to "found" list
+                self.found(hash)
+                # Saves result to results.txt
+                self.save_result(result)
                 return result
-        if flag == False:
-            notfound = "Hash "+hash+" not found."
+        if occurrences == False:
+            notfound = "Hash %s not found." % hash
             print notfound
+            # Saves hash to "not found" list
+            self.not_found(hash)
 
-    def crack_single_hash(self):
-        wordlist = []
-        opener = urllib2.build_opener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-
+    def crack_hashes(self):
         for hash in self.hashes:
-            try:
-                sivu = opener.open('http://www.google.com/search?q='+hash+'', timeout=10)
-            except urllib2.URLError:
-                print "URL error."
-                continue
-            for word in re.split(r"\s+", str(sivu.read())):
-                    wordlist.append(word)
-            save = self.dictionary_attack(hash, wordlist)
-            if save:
-                f = open('output.txt', 'a+')
-                f.write(save + '\n')  # saves output to file
+            self.google_crack(hash)
+            
+    def google_crack(self, hash):
+        """ Do a google search for the hash and add found words to the wordlist """
+        try:
+            page_handle = self.opener.open('http://www.google.com/search?q=%s' \
+                                    % hash, timeout=10)
+        except urllib2.URLError as e:
+            self.logger.warn("URL error %s", e.message)
+            return
+            
+        # Read page to a string
+        google_wordlist = re.split(r"\s+", str(page_handle.read()))
+        
+        for word in google_wordlist:
+            self.wordlist.append(word)
+        result = self.dictionary_attack(hash, self.wordlist)
+        return result
 
 # usage example
 
@@ -57,6 +102,6 @@ if __name__ == "__main__":
     else:
         filename = raw_input("File: ")  # file to search for hashes
     instance = PyCrack(filename = filename)
-    instance.crack_single_hash()
+    instance.crack_hashes()
 
 
