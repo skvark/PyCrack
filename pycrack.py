0a613a2ee571bf8008f321acec86cb11945179e2
Jussi <jussir@gmail.com>
2011-11-18 19:37:28 +0200

    - Added some pythonish conventions, pep8 support, changed tabs to spaces.

--- pycrack.py
+++ pycrack.py
@@ -1,53 +1,62 @@
+#!/usr/bin/env python
 import hashlib
 import urllib2
 import re
+from sys import argv
 
 # Very simple md5 cracking with Google, idea taken from 
-# https://github.com/juuso/BozoCrack/blob/master/bozocrack.rb
+# https://github.com/juuso/BozoCrack/blob/master/BozoCrack.rb
 
 # Feel free to use =)
 
-class BozoCrack:
+class PyCrack:
+    """ PyCrack tries to google each hash trying to find
+	an occurrence """
+    def __init__(self, filename):
+        self.hashes = []
+        fhandle = open(filename,'r')
+        hash_list = fhandle.read()
+        self.hashes = re.findall(r"([a-fA-F\d]{32})", str(hash_list))
+        self.hashes = list(set(self.hashes))  # removes duplicates
 
-	def __init__(self):
-		self.hashes = []
-		filename = raw_input("File: ") # file to search for hashes
-		filee = open(filename,'r')
-		filee = filee.read()
-		self.hashes = re.findall(r"([a-fA-F\d]{32})", str(filee))
-		self.hashes = list(set(self.hashes)) # removes duplicates
+    def dictionary_attack(self, hash, wordlist):
+        flag = False
+        for key in wordlist:
+            if hashlib.md5(key).hexdigest() == hash:
+                result = "%s:%s" % (hash,key,)
+                print result
+                flag = True
+                return result
+        if flag == False:
+            notfound = "Hash "+hash+" not found."
+            print notfound
 
-	def dictionary_attack(self, hash, wordlist):
-		flag = False
-		for key in wordlist:
-			if hashlib.md5(key).hexdigest() == hash:
-				result = ""+hash+":"+key+""
-				print result
-				return result
-				flag = True
-				break
-		if flag == False:
-			notfound = "Hash "+hash+" not found."
-			print notfound
-			return notfound
-		
-	def crack_single_hash(self):
-		wordlist = []
-		for hash in self.hashes:
-			opener = urllib2.build_opener()
-			opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-			try:
-				sivu = opener.open('http://www.google.com/search?q='+hash+'', timeout=10)
-			except urllib2.URLError:
-				print "URL error."
-				pass
-			for word in re.split(r"\s+", str(sivu.read())):
-				wordlist.append(word)
-			save = self.dictionary_attack(hash, wordlist)
-			f = open('output.txt', 'a+')
-			f.write(save + '\n') # saves output to file
-			
+    def crack_single_hash(self):
+        wordlist = []
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+        for hash in self.hashes:
+            try:
+                sivu = opener.open('http://www.google.com/search?q='+hash+'', timeout=10)
+            except urllib2.URLError:
+                print "URL error."
+                continue
+            for word in re.split(r"\s+", str(sivu.read())):
+                    wordlist.append(word)
+            save = self.dictionary_attack(hash, wordlist)
+            if save:
+                f = open('output.txt', 'a+')
+                f.write(save + '\n')  # saves output to file
+
 # usage example
 
-instance = BozoCrack()
-instance.crack_single_hash()
+if __name__ == "__main__":
+    if len(argv) > 1:
+	filename = argv[1]
+    else:
+        filename = raw_input("File: ")  # file to search for hashes
+    instance = PyCrack(filename = filename)
+    instance.crack_single_hash()
+
+
